3f332f4c677a311179b4f375f2881f125ffe61f3
artslimedev <102567983+artslimedev@users.noreply.github.com>
2025-05-13 15:25:35 -0400

    Pushing to resolve vercel deploy

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -4,7 +4,6 @@ import React from "react";
 const Avatar = () => {
   return (
     <div className="rounded-full h-8 w-8 hover:h-10 hover:w-10">
-      {/* <img src="" className="rounded-full h-8 w-8 hover:h-10 hover:w-10" /> */}
       <Image
         src="https://avatar.vercel.sh/59283.png"
         height={32}
